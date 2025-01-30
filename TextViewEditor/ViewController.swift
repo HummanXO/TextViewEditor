0798a38664b277f75c17ed9da76957eddda896bb
HummanXO <rxkolchin@gmail.com>
2025-01-30 15:32:12 +0700

    Initial Commit

---
+++ TextViewEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextViewEditor
+//
+//  Created by Alexandr on 30/01/2025.
+//  Copyright © 2025 Alexandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
